7d4dde5e641716b3db60f6039b361e71aa0f875f
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2024-07-16 00:01:23 +0530

    Added container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header} from './containers'
+import { Navbar, Brand,CTA } from './components'
+import './App.css'
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div class="App">
+      <div class="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+        <Brand/>
+        <WhatGPT3/>
+        <Feature/>
+        <Possibility/>
+        <CTA/>
+        <Blog/>
+        <Footer/>
+
     </div>
   )
 }
