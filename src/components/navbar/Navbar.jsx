a2d76f41a348f2fcc05cb3694983d0dcc4d39f52
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2024-07-29 00:23:44 +0530

    error: Solved error of Navbar,jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,15 +3,6 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
-const Menu = () => {
-  <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#whatGPT3">What is GPT?</a></p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#feature">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
-  </>
-}
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
